ec81fb2fa1f2c2906f211bbdf9c767ce173bdc5f
Christine-design-web <wakuthiichristine5@gmail.com>
2023-05-16 17:28:30 +0300

    executed

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
 
-for element in my_list=[]:
-        if isinstance(element, int):
+for element in range(len(my_list)):
+       
             print("Integer: {}".format(element))
