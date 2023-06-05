a45ee79a849d09653bdeb29e77e1f7c8336bc515
Christine-design-web <wakuthiichristine5@gmail.com>
2023-06-06 01:24:38 +0300

    second commit

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module 1-rectangle
+"""Module 2-rectangle
 Defines a Rectangle class.
 """
 
@@ -52,3 +52,21 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Calculates the area of a Rectangle instance
+
+        Returns:
+            Area of the the rectangle, given by height * width
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculates the perimeter of a Rectangle instance
+
+        Returns:
+            Perimeter of the rectangle, given by 2 * (height + width)
+        """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
