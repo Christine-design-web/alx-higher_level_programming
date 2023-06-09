c63d02d7466a70f9434dfe0ddb79e3ef7697046a
Christine-design-web <wakuthiichristine5@gmail.com>
2023-06-09 16:21:51 +0300

    testing

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,19 @@
+#!/usr/bin/python3
+"""
+Module say_my_name
+Prints a given first name and last name.
+"""
+
+
 def say_my_name(first_name, last_name=""):
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or last_name must be a string")
+    """Prints a string with <first_name>
+    and <last_name>.
+    """
 
-    full_name = "My name is " + first_name + " " + last_name
-    print(full_name)
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
 
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
