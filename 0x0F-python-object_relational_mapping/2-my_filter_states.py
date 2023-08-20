49788b9a83a813c3f6b3281caedf0a9aa1a0b3ec
Christine-design-web <wakuthiichristine5@gmail.com>
2023-08-20 23:35:43 +0300

    executable

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-return matching states; safe from MySQL injections
+return matching states
 parameters given to script: username, password, database, state to match
 """
 
@@ -8,7 +8,6 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-
     # connect to database
     db = MySQLdb.connect(host="localhost",
                          port=3306,
@@ -20,10 +19,10 @@ if __name__ == "__main__":
     cursor = db.cursor()
     sql_cmd = """SELECT *
                  FROM states
-                 WHERE name=%s ORDER BY id ASC"""
-    cursor.execute(sql_cmd, (argv[4],))
-
+                 WHERE name LIKE '{:s}' ORDER BY id ASC""".format(argv[4])
+    cursor.execute(sql_cmd)
     for row in cursor.fetchall():
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cursor.close()
     db.close()
