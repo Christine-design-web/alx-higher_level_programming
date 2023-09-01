9adce4cc9c762bd4a2932f5c389f09c61e56d99e
Christine-design-web <wakuthiichristine5@gmail.com>
2023-09-01 14:46:21 +0300

    error

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Sends a POST request to a given URL with a given email.
 Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
 import sys
 import urllib.parse
